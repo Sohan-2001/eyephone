8d996037c101f37c86ec4207f2c551a574bc121c
Sohan Karfa <sohan100karfa@gmail.com>
2025-06-24 18:52:53 +0000

    Make this the wallpaper

--- src/components/widget-board.tsx
+++ src/components/widget-board.tsx
@@ -11,8 +11,12 @@ export default function WidgetBoard() {
     <div
       className="flex-1 w-full h-full overflow-y-auto overflow-x-hidden relative"
       style={{
-        backgroundColor: '#000',
-        backgroundImage: 'radial-gradient(ellipse 75% 100% at 50% 0%, hsl(245, 100%, 25%), transparent 50%), radial-gradient(ellipse 75% 100% at 50% 100%, hsl(260, 40%, 20%), transparent 50%)',
+        backgroundColor: 'hsl(233, 15%, 12%)',
+        backgroundImage: `
+          radial-gradient(ellipse 100% 70% at 50% 0%, hsl(250, 15%, 28%), transparent 50%),
+          radial-gradient(ellipse 80% 60% at 50% 100%, hsl(220, 100%, 70%), transparent 40%),
+          radial-gradient(ellipse 80% 58% at 50% 102%, hsl(233, 15%, 12%) 39%, transparent 40%)
+        `,
       }}
     >
       <div className="grid grid-cols-4 grid-rows-6 gap-4 p-4 pt-12 h-[calc(100%-8rem)]">
