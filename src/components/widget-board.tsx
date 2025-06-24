3da552d6e0e18af7d5e651b0349e9dc6ed5a9954
Sohan Karfa <sohan100karfa@gmail.com>
2025-06-24 19:10:52 +0000

    Ok, I understand it's difficult to create a cool wallpaper completely by

--- src/components/widget-board.tsx
+++ src/components/widget-board.tsx
@@ -6,24 +6,11 @@ import { ConnectivityWidget } from "@/components/widgets/connectivity-widget";
 import { PhotosAppWidget } from "@/components/widgets/photos-app-widget";
 import { SiriWidget } from "@/components/widgets/siri-widget";
 
-const FloatingBubbles = () => (
-    <div className="bubbles">
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-        <div className="bubble"></div>
-    </div>
-);
-
 export default function WidgetBoard() {
   return (
     <div
       className="flex-1 w-full h-full overflow-y-auto overflow-x-hidden relative wallpaper-container"
     >
-      <FloatingBubbles />
       <div className="grid grid-cols-4 grid-rows-6 gap-4 p-4 pt-12 h-[calc(100%-8rem)] relative z-10">
         
         <div className="col-span-4 row-span-1">
